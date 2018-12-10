28bd095599207e0ac4e8263dafbbc8e034353056
Paulo Vitor Magacho da Silva <pvmagacho@gmail.com>
2018-12-10 17:36:43 -0200

    Updated demo

--- demo/app/main-page.ts
+++ demo/app/main-page.ts
@@ -1,5 +1,7 @@
 import * as observable from 'tns-core-modules/data/observable';
 import * as pages from 'tns-core-modules/ui/page';
+import * as fs from 'tns-core-modules/file-system';
+import { UploadTus } from 'nativescript-tus-upload';
 import {HelloWorldModel} from './main-view-model';
 
 // Event handler for Page 'loaded' event attached in main-page.xml
@@ -7,4 +9,26 @@ export function pageLoaded(args: observable.EventData) {
     // Get the event sender
     let page = <pages.Page>args.object;
     page.bindingContext = new HelloWorldModel();
+}
+
+export function onTap(args: observable.EventData): void {
+  const folder = fs.knownFolders.documents().getFolder('temp');
+  const file = folder.getFile('temp.txt');
+  file.writeTextSync('A TEXT TO WRITE A FILE\nA TEXT TO WRITE A FILE\nA TEXT TO WRITE A FILE\n');
+
+  const url = 'https://tus-tmp-pvms.herokuapp.com/';
+
+  const uploadTus = new UploadTus();
+  console.log('WILL UPLOAD');
+  uploadTus.uploadFile({
+    path: file.path,
+    url: url,
+    headers: { 'Authorization': 'Bearer TEST' }
+  }, (response: any) => {
+    if (response.error) {
+      console.error(response.error);
+      return;
+    }
+    console.log(`DID UPLOAD ${JSON.stringify(response, null, 2)}`);
+  });
 }
--- demo/app/main-view-model.ts
+++ demo/app/main-view-model.ts
@@ -1,32 +1,8 @@
 import { Observable } from 'tns-core-modules/data/observable';
-import { UploadTus } from 'nativescript-tus-upload';
-import * as fs from 'tns-core-modules/file-system';
 
 export class HelloWorldModel extends Observable {
-  public message: string;
-  private uploadTus: UploadTus;
 
   constructor() {
     super();
-
-    const folder = fs.knownFolders.documents().getFolder('temp');
-    const file = folder.getFile('temp.txt');
-    file.writeTextSync('A TEXT TO WRITE A FILE\nA TEXT TO WRITE A FILE\nA TEXT TO WRITE A FILE\n');
-
-    const url = 'https://tus-tmp-pvms.herokuapp.com/';
-
-    console.log('WILL UPLOAD');
-    this.uploadTus = new UploadTus();
-    this.uploadTus.uploadFile({
-      path: file.path,
-      url: url,
-      headers: { 'Authorization': 'Bearer TEST' }
-    }, (response: any) => {
-      if (response.error) {
-        console.error(response.error);
-        return;
-      }
-      console.log(`DID UPLOAD ${JSON.stringify(response, null, 2)}`);
-    });
   }
 }
